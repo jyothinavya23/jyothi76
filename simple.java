86a95d98c838336504a37e55c197b414dbcec9ec
Kavya <159226897+jyothinavya23@users.noreply.github.com>
2024-07-26 12:03:00 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -2,8 +2,9 @@ public class simple
 {
 	public static void main(String[]args)
 	{
-		system.ot.println("welcome to java program");
-		system.ot.println("welcome to java program");
+		system.out.println("java");
+		system.ot.println("welcome to spt");
+		system.out.println("tumkurur");
 		
 	}
 }
